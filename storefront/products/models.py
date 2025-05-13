6304495631154abae3d2119a9e3de45014d4c0da
Zakhar Frolov <frolov_za@MacBook-Air-Zakhar.local>
2025-05-13 15:28:53 +0300

    Изменен размер загружаемых изображений

--- storefront/products/models.py
+++ storefront/products/models.py
@@ -6,22 +6,7 @@ class Product(models.Model):
     barcode = models.CharField(max_length=50, help_text="Артикул для тары 1 литр") 
     barcode15 = models.CharField(max_length=50, help_text="Артикул для тары 1.5 литра")
     description = models.TextField(blank=True, help_text="Описание товара, отображается при выборе товара")
-    image = CropperImageField(dimensions=(350, 350), upload_to='product_images/')
-    #image = models.ImageField(upload_to='product_images/')
+    image = CropperImageField(dimensions=(250, 250), upload_to='product_images/')
 
     def __str__(self):
         return self.name
-
-# class DeviceType(models.TextChoices):
-#     USB = 'USB', 'USB'
-#     NETWORK = 'NETWORK', 'Network'
-
-# class Printer(models.Model):
-#     name = models.CharField(max_length=100)
-#     location = models.CharField(max_length=50) 
-#     type = models.CharField(max_length=10, choices=DeviceType.choices, default=DeviceType.USB)
-#     in_use = models.BooleanField(default=False)
-
-#     def __str__(self):
-#         return self.name
-
